31471e705e975fe7474f512eba22f97773bbf988
Sean McDermott <mcderm.sean@gmail.com>
2017-03-08 15:06:42 -0800

    started working on zip validation for BILLING, cleaned up checkout.html for better navigation within IDE

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -8,8 +8,18 @@ $('form').on('submit', function(e){
   e.preventDefault();
 
   if ($('#zipCode-ship').val().length !== 5 && $('#zipCode-ship').val().length !== 9){
-    $('.zip').addClass('has-error');
+    $('.zip-ship').addClass('has-error');
   }else if($('#zipCode-ship').val().length === 5 || $('#zipCode-ship').val().length === 9){
-    $('.zip').removeClass('has-error');
+    $('.zip-ship').removeClass('has-error');
   }
 });
+
+// $('form').on('submit', function(e){
+//   e.preventDefault();
+//
+//   if ($('#zipCode-bill').val().length !== 5 && $('#zipCode-bill').val().length !== 9){
+//     $('.zip').addClass('has-error');
+//   }else if($('#zipCode-bill').val().length === 5 || $('#zipCode-bill').val().length === 9){
+//     $('.zip').removeClass('has-error');
+//   }
+// });
