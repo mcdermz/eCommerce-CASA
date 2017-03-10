e297dcc24e1d22ac4027d9a36f7b99c636110b13
Sean McDermott <mcderm.sean@gmail.com>
2017-03-10 10:49:57 -0800

    slider arrow buttons added, right-arrow works but left-arrow does not work

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,5 +1,5 @@
 console.log('sanity!!!!!');
-// $('#feature-row').append(arrOfCol[0], arrOfCol[1], arrOfCol[2]);
+
 
 function myMap() {
     var mapCanvas = document.getElementById("map");
@@ -10,87 +10,67 @@ function myMap() {
     var map = new google.maps.Map(mapCanvas, mapOptions);
 }
 
-var colOpenTag = `<div class="col-xs-4">`
-var imgFeature = `<img class="image-feature" src="http://fillmurray.com/300/300" alt="Bill Murray Picture">`
-var buttonShoppingCart = `<div><h6>ADD TO CART</h6></div>`
-var addCartText= `<a href="#" class="btn addCartText" style="display:none">Add Cart Item</a>`
+var colOpenTag = `<div class="col-xs-4">`;
+var imgFeature = `<img class="image-feature"; src="http://fillmurray.com/300/300" alt="Bill Murray Picture">`;
+var buttonShoppingCart = `<div><h6>ADD TO CART</h6></div>`;
+var addCartText= `<a href="#" class="btn addCartText" style="display:none">Add Cart Item</a>`;
 
 
 
 var featureCol = []
   for (var i = 0; i < products.length; i++) {
-    featureCol.push(colOpenTag + imgFeature + addCartText + `<p> ${products[i].id}</p>` + `<p> ${products[i].ratings}</p>` + `<p> ${products[i].price}</p>` + `</div>`)
-
+    featureCol.push(colOpenTag + imgFeature + addCartText + `<p> ${products[i].id}</p>` + `<p> ${products[i].ratings}</p>` + `<p> ${products[i].price}</p>` + `</div>`);
   }
 
-  console.log("Hello?", $('#feature-row').append(featureCol[0], featureCol[1], featureCol[2]));
-
-
-  // var featureWrapper = `<div class="wrapper-feature"><span class="text-feature">ADD TO CART</span>${imgFeature}</div>`
-
   $('.image-feature').hover(function(){
-    $(this).addClass('image-feature')
-    $(this).siblings('.addCartText').addClass('display', 'block')
+    $(this).addClass('image-feature');
+    $(this).siblings('.addCartText').addClass('display', 'block');
     $(this).siblings('.addCartText').show();
 
-    // '.addCartText').toggleClass('hidden')
-
-
   }, function(){
-    $(this).removeClass('image-feature')
+    $(this).removeClass('image-feature');
     $(this).siblings('.addCartText').hide();
-    // $('.addCartText').attr('opacity', '1')
   })
 
-
-
-
-  // $('.mouseenter').on('mouseenter', function () {
-  //   console.log('!')
-  //   $(this).toggleClass('orange')
-
-
-
-
-
-
-
-
-// for (var i = 0; i < products.length; i++) {
-//   arrOfCol.push(colOpen + br + picture + `<p>Id: ${products[i].id}</p>` + `<p>Size: ${products[i].size}</p>` + `<p>Description: ${products[i].description}</p>` + divClose)
-// }
-
-
-// var myInterval = setInterval(function(){
-//
-//   // console.log(new Date());
-//
-// }, 1000);
-//
-//
-// $('#slider .slides').animate({'margin-left': -720}, 1000);
-
+// carousel
 $(function(){
   var $slider = $('#slider .slides');
   var currentSlide = 1;
-
-  // var width = 720;
   var animateSpeed = 1000;
-  var pause = 3000;
+  var pause = 6000;
+  function caroselMove(next){
+    if (next){
+      $slider.animate({'margin-left': '-=1440'}, animateSpeed, function(){
+        currentSlide++;
+        if(currentSlide === $('.slide').length){
+          currentSlide = 1;
+          $slider.css('margin-left', 0);
+        }
+      })
+    } else {
+      $slider.animate({'margin-right': '+=1440'}, animateSpeed, function(){
+        currentSlide--;
+        if(currentSlide === 0){
+          currentSlide = $('.slide').length;
+          $slider.css('margin-right', 0);
+        }
+      })
+    }
+  }
 
   setInterval(function(){
-    $slider.animate({'margin-left': '-=1440'}, animateSpeed, function(){
-      currentSlide++;
-      if(currentSlide === $('.slide').length){
-        currentSlide = 1;
-        $slider.css('margin-left', 0);
-      }
-    });
+    caroselMove(next)
   }, pause);
 
-
-  //listen for mouseenter and pause
-  //resume on mouseleave
-
-
+  $('aside a').on('click', function(){
+    if ($(this).attr('id') === 'next'){
+      let next = true;
+      caroselMove(next);
+    }
+    if ($(this).attr('id') === 'prev'){
+      let next = false;
+      caroselMove(next);
+    }
+  });
+  // arrow keys
 });
